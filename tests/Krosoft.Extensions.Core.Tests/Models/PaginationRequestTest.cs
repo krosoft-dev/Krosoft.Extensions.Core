2bc75db4e1fb272bff482b76d9d974e66f4d5947
Kévin ROBERT <kevin.robert.kr@gmail.com>
2025-08-22 11:22:26 +0200

    Ajout PaginationRequestTest

---
+++ tests/Krosoft.Extensions.Core.Tests/Models/PaginationRequestTest.cs
@@ -0,0 +1,22 @@
+﻿using JetBrains.Annotations;
+using Krosoft.Extensions.Core.Models;
+
+namespace Krosoft.Extensions.Core.Tests.Models;
+
+[TestClass]
+[TestSubject(typeof(PaginationRequest))]
+public class PaginationRequestTest
+{
+    [TestMethod]
+    public void Empty()
+    {
+        var paginationRequest = new PaginationRequest();
+
+        Check.That(paginationRequest).IsNotNull();
+        Check.That(paginationRequest.PageNumber).IsEqualTo(0);
+        Check.That(paginationRequest.PageSize).IsEqualTo(0);
+        Check.That(paginationRequest.SortBy).IsNotNull();
+        Check.That(paginationRequest.SortBy).IsEmpty();
+        Check.That(paginationRequest.Text).IsNull();
+    }
+}
