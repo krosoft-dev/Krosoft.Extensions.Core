4a78b81a5de7f29106b812991c46f913249fe7f3
Kévin ROBERT <kevin.robert.kr@gmail.com>
2025-08-22 15:13:53 +0200

    Ajout IPaginationDto

---
+++ src/Krosoft.Extensions.Core/Models/Dto/IPaginationDto.cs
@@ -0,0 +1,12 @@
+﻿namespace Krosoft.Extensions.Core.Models.Dto;
+
+public interface IPaginationDto
+{
+    public int? PageNumber { get; set; }
+
+    public int? PageSize { get; set; }
+
+    public string[]? SortBy { get; set; }
+
+    public string? Text { get; set; }
+}
