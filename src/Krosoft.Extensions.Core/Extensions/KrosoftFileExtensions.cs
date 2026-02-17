ba20d22c5eb2a27f6b285d69ac9e250c2364a239
Kévin ROBERT <kevin.robert.kr@gmail.com>
2026-02-17 15:25:36 +0100

    Add KrosoftFileExtensions class with ToMultipartFormDataContent method

---
+++ src/Krosoft.Extensions.Core/Extensions/KrosoftFileExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Krosoft.Extensions.Core.Models;
+
+namespace Krosoft.Extensions.Core.Extensions;
+
+public static class KrosoftFileExtensions
+{
+    public static MultipartFormDataContent ToMultipartFormDataContent(this KrosoftFile file)
+    {
+        var content = new MultipartFormDataContent();
+        content.Add(new ByteArrayContent(file.Content), "file", file.Name);
+        return content;
+    }
+}
