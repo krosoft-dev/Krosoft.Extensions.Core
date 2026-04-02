4ca676b63413903af8a88a21ff041062e55a0706
Kévin ROBERT <kevin.robert.kr@gmail.com>
2026-04-02 12:19:18 +0200

    Add IPaginationDto dependency to PaginationRequest and enhance From method

--- src/Krosoft.Extensions.Core/Models/PaginationRequest.cs
+++ src/Krosoft.Extensions.Core/Models/PaginationRequest.cs
@@ -1,4 +1,6 @@
-﻿namespace Krosoft.Extensions.Core.Models;
+﻿using Krosoft.Extensions.Core.Models.Dto;
+
+namespace Krosoft.Extensions.Core.Models;
 
 public record PaginationRequest : ISearchPaginationRequest
 {
@@ -6,4 +8,12 @@ public record PaginationRequest : ISearchPaginationRequest
     public int PageSize { get; set; } = 10;
     public ISet<string> SortBy { get; set; } = new HashSet<string>();
     public string? Text { get; set; }
+
+    public static PaginationRequest From(IPaginationDto dto) => new()
+    {
+        Text = dto.Text,
+        PageNumber = dto.PageNumber ?? 1,
+        PageSize = dto.PageSize ?? 10,
+        SortBy = dto.SortBy != null ? dto.SortBy.ToHashSet() : new HashSet<string>()
+    };
 }
