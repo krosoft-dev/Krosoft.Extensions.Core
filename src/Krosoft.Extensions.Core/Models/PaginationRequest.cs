c43fcf43a17e4fe0cd74eadf1a339df5df7891e8
Kévin ROBERT <kevin.robert.kr@gmail.com>
2026-04-02 13:20:53 +0200

    Remove unused IPaginationDto dependency and From method from PaginationRequest

--- src/Krosoft.Extensions.Core/Models/PaginationRequest.cs
+++ src/Krosoft.Extensions.Core/Models/PaginationRequest.cs
@@ -1,6 +1,4 @@
-﻿using Krosoft.Extensions.Core.Models.Dto;
-
-namespace Krosoft.Extensions.Core.Models;
+﻿namespace Krosoft.Extensions.Core.Models;
 
 public record PaginationRequest : ISearchPaginationRequest
 {
@@ -8,12 +6,4 @@ public record PaginationRequest : ISearchPaginationRequest
     public int PageSize { get; set; } = 10;
     public ISet<string> SortBy { get; set; } = new HashSet<string>();
     public string? Text { get; set; }
-
-    public static PaginationRequest From(IPaginationDto dto) => new()
-    {
-        Text = dto.Text,
-        PageNumber = dto.PageNumber ?? 1,
-        PageSize = dto.PageSize ?? 10,
-        SortBy = dto.SortBy != null ? dto.SortBy.ToHashSet() : new HashSet<string>()
-    };
 }
