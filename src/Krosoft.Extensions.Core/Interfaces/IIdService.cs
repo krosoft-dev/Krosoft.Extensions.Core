688dc86ee9f1569d8f9dd624fb47e28d89d23b96
Kévin ROBERT <kevin.robert.kr@gmail.com>
2026-08-27 23:26:23 +0200

    Ajout des commentaires XML de documentation sur les interfaces de Krosoft.Extensions.Core

--- src/Krosoft.Extensions.Core/Interfaces/IIdService.cs
+++ src/Krosoft.Extensions.Core/Interfaces/IIdService.cs
@@ -1,7 +1,19 @@
 namespace Krosoft.Extensions.Core.Interfaces;
 
+/// <summary>
+/// Service de génération d'identifiants uniques.
+/// </summary>
 public interface IIdService
 {
+    /// <summary>
+    /// Génère un nouvel identifiant <see cref="Guid" />.
+    /// </summary>
+    /// <returns>Un nouveau <see cref="Guid" />.</returns>
     Guid NewGuid();
+
+    /// <summary>
+    /// Génère un nouvel identifiant <see cref="Guid" /> en version 7 (ordonnable dans le temps).
+    /// </summary>
+    /// <returns>Un nouveau <see cref="Guid" /> en version 7.</returns>
     Guid NewGuidVersion7();
 }
--- src/Krosoft.Extensions.Core/Interfaces/IXmlLoaderService.cs
+++ src/Krosoft.Extensions.Core/Interfaces/IXmlLoaderService.cs
@@ -1,9 +1,20 @@
-﻿using System.Xml;
+using System.Xml;
 using Krosoft.Extensions.Core.Models;
 
 namespace Krosoft.Extensions.Core.Interfaces;
 
+/// <summary>
+/// Service de chargement de documents XML à partir d'un fichier.
+/// </summary>
 public interface IXmlLoaderService
 {
+    /// <summary>
+    /// Charge le contenu d'un fichier dans un <see cref="XmlDocument" />.
+    /// </summary>
+    /// <param name="xmlFile">Le fichier XML à charger.</param>
+    /// <returns>
+    /// Un <see cref="Result{XmlDocument}" /> contenant le document chargé en cas de succès,
+    /// ou l'erreur rencontrée si le fichier est vide ou invalide.
+    /// </returns>
     Result<XmlDocument> Load(KrosoftFile xmlFile);
 }
